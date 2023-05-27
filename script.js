8f1a67f7778b684325ba9f6e926e9ee6153f0462
Irfan <116108210+irfanraizel@users.noreply.github.com>
2023-05-27 17:51:58 +0700

    Upload repo suit jepang

---
+++ script.js
@@ -0,0 +1,11 @@
+// comp received random number
+// const comp = Math.random()*10;
+// if ( comp <= 3.5 ) {
+//     comp = 'batu';
+// } else if ( comp > 3.5 && comp <= 7 ) {
+//     comp = 'gunting';
+// } else if ( comp > 7 ) {
+//     comp = 'kertas';
+// }
+
+// player input click
